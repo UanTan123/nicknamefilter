1225ae67a1a04c9327e7e2177a10669608753201
UN10 <62202857+UanTan123@users.noreply.github.com>
2023-09-02 20:15:50 +0900

    First upload source code.
    Add source code.

---
+++ nicknamefilter/Handlers/PlayerEvents.cs
@@ -0,0 +1,59 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+//using EPlayer = Exiled.API.Features.Player;
+using EPlugin = nicknamefilter.Plugin;
+using Exiled.Events.EventArgs.Player;
+
+namespace nicknamefilter
+{
+    public class PlayerEvents
+    {
+        public PlayerEvents(EPlugin plugin) => this.plugin = plugin;
+        private readonly EPlugin plugin;
+
+        public static string NicknameFiltering(string nickname)
+        {
+            string lowernick = nickname.ToLower();
+            return lowernick.
+                Replace("twitch", "").
+                Replace("youtube", "").
+                Replace("트위치", "").
+                Replace("유튜브", "").
+                Replace("운영자", "").
+                Replace("씨발", "").
+                Replace("시발", "").
+                Replace("개새끼", "").
+                Replace("개새기", "").
+                Replace("엄마", "").
+                Replace("아빠", "").
+                Replace("윤석열", "").
+                Replace("노무현", "").
+                Replace("운지", "").
+                Replace("병신", "").
+                Replace("섹스", "");
+        }
+
+        public void OnJoined(JoinedEventArgs ev)
+        {
+            string nickname = ev.Player.Nickname;
+            string newnickname = NicknameFiltering(nickname);
+            if (nickname != newnickname)
+            {
+                ev.Player.ShowHint("<color=red>당신의 닉네임은 홍보성닉네임이 포함되어있어서 변경되었습니다.</color>", 5);
+                ev.Player.DisplayNickname = newnickname;
+                Log.Info($"{ev.Player.Nickname}({ev.Player.UserId})님의 닉네임이 변경되었습니다.(홍보성 닉네임)\n새로운 닉네임 : {newnickname}");
+                if (ev.Player.DisplayNickname == "" || ev.Player.DisplayNickname == " ")
+                {
+                    ev.Player.ShowHint("<color=red>당신의 닉네임은 홍보성닉네임이 포함되어있어서 변경되었습니다.</color>", 5);
+                    ev.Player.DisplayNickname = newnickname + "UnknownNickName(Filterling by Plugin)";
+                    Log.Info($"{ev.Player.Nickname}({ev.Player.UserId})님의 닉네임이 변경되었습니다.(홍보성 닉네임)\n새로운 닉네임 : {newnickname+ "UnknownNickName(Filterling by Plugin)"}");
+                }
+            }
+        }
+    }
+}
---
+++ nicknamefilter/Plugin.cs
@@ -0,0 +1,53 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Player = Exiled.Events.Handlers.Player;
+
+namespace nicknamefilter
+{
+    public class Plugin : Plugin<Config>
+    {
+        private static readonly Lazy<Plugin> LazyInstance = new Lazy<Plugin>(() => new Plugin());
+        public static Plugin Instance => LazyInstance.Value;
+
+        public override PluginPriority Priority { get; } = PluginPriority.Low;
+
+        public PlayerEvents player;
+
+        public override string Prefix => "[NICKNAME FILTER]";
+        public override string Author => "UN10";
+
+        private Plugin()
+        {
+
+        }
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            RegisterEvents();
+            Log.Info("플러그인이 켜졌습니다.");
+        }
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            UnregisterEvents();
+            Log.Info("플러그인이 꺼졌습니다.");
+        }
+
+        public void RegisterEvents()
+        {
+            player = new PlayerEvents(this);
+            Player.Joined += player.OnJoined;
+        }
+        public void UnregisterEvents()
+        {
+            Player.Joined -= player.OnJoined;
+        }
+    }
+}
